a49daf017dd2633f6740afc6bdaea150d34fafd6
Gabriel Bianchi Britto (Bolsista) <gabriel.britto@cefetes.br>
2026-02-23 12:56:18 -0300

    feat: create Email Listener

--- email-notification-system-backend/src/main/java/com/example/demo/rabbit/listener/EmailListener.java
+++ email-notification-system-backend/src/main/java/com/example/demo/rabbit/listener/EmailListener.java
@@ -1,15 +1,21 @@
 package com.example.demo.rabbit.listener;
 
+import com.example.demo.dto.email.request.EmailRequest;
+import com.example.demo.services.EmailService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
 @Component
-class EmailListener
+@RequiredArgsConstructor
+public class EmailListener
 {
+    private final EmailService emailService;
 
     @RabbitListener(queues = "email.queue")
-    public void send()
+    public void send(EmailRequest request)
     {
-        System.out.println("enviando email...");
+        System.out.println("enviando email para o " + request.to());
+        emailService.send(request);
     }
 }
